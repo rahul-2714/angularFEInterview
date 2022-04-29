7b5098e96a304c3c38790070c066df4ff04efd1a
rahul-2714 <89128513+rahul-2714@users.noreply.github.com>
2022-04-29 12:06:00 +0530

    Changed app component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, VERSION } from '@angular/core';
 import { animals } from './data/animals-data';
 
 @Component({
-	selector: 'app-root',
+	selector: 'my-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
 })
